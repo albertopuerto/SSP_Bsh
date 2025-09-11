cf333c2cc59ad286e6bf2c9bd321d28c3edc41d3
Andres Pacheco <andres.pacheco@adesso.es>
2025-09-11 18:40:10 +0200

    first visual changes for credit limit

--- force-app/main/default/lwc/creditLimit/creditLimit.js
+++ force-app/main/default/lwc/creditLimit/creditLimit.js
@@ -38,6 +38,8 @@ export default class creditLimit extends LightningElement {
 
    @track recordData;
 
+   @track buttonTitle = '';
+
     @wire(getRecord, { recordId: "$recordId", fields: ["Account.txt_AthenaUUID__c"] })
     wiredAccount({ data, error }) {
     
@@ -51,23 +53,31 @@ export default class creditLimit extends LightningElement {
   }
 
    detectObject(){
+
+    this.emptyVariables();
         
     if(this.objectApiName == 'Account'){
+        
         this.accountRecord = true;
     
         if (this.recordData.fields.txt_AthenaUUID__c.value !=null) {
         
             this.disableButton=false;
+            this.buttonTitle= '';
+            
         }else{
        
             this.disableButton=true; 
+            this.buttonTitle= 'Please fill the Athena ID before checking the Credit Limit';
+           
         }
     /*
             checkAthenaId({objectRecordId : this.recordId})
             .then(result => {
                
-            
+           
             })*/
+        
     }else if(this.objectApiName == 'SBQQ__Quote__c'){
         this.quoteRecord = true;
     }else if(this.objectApiName == 'Order'){
@@ -97,9 +107,41 @@ export default class creditLimit extends LightningElement {
             this.authToken = result;
         })
     }
+
+    
+    refreshCallMethodWorthiness(){
+        this.emptyVariables();
+        this.isLoading=true;
+      //  alert('quoteRecord '+ this.quoteRecord);
+      //  alert('orderRecord '+ this.orderRecord);
+
+        if(this.quoteRecord== true){
+      
+            this.getQuoteCreditLimit();
+        
+        }else if(this.orderRecord==true){
+
+            this.getOrderCreditLimit();
+        }
+    }
+
+    emptyVariables(){
+        this.errorMessage= '';
+        this.quoteLimitMessage= '';
+        this.orderLimitMessage= '';
+        this.accountLimit= 0;
+        this.showAccountLimit= false;
+        this.showError= false;
+        this.quoteLimitSuccess = false;
+        this.quoteLimitError= false;
+        this.quoteLimitMessage= '';
+        this.orderLimitSuccess= false;
+        this.orderLimitError= false;
+        this.orderLimitMessage= '';
+    }
    
    // call to a class for the three methods one class apart with the three calls to the three endpoints and the respective errors to nebula, If there is any error or failure send a toast
-   getAccountCreditLimit(){
+getAccountCreditLimit(){
         
 
          getObjectCurrency({isAccount:this.accountRecord,isQuote: this.quoteRecord, isOrder: this.orderRecord,objectRecordId : this.recordId})
@@ -166,7 +208,7 @@ export default class creditLimit extends LightningElement {
                     this.orderLimitSuccess= true;
                 }
               
-                
+                this.isLoading= false;
             })
    }
 }
